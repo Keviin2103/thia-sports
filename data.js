7f8d8ed79664d78583e56ce102760cb26fc151ea
Keviin2103 <kevinosq@gmail.com>
2026-04-24 23:53:56 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const nhlPicks = [];
+const nbaPicks = [];
+const mlbPicks = [
+  {
+    "partido": "Yankees vs Red Sox",
+    "hora": "07:10 PM VEN",
+    "principal": {"pick": "Yankees ML", "cuota": 1.61, "ev": "+9.5%", "stake": "2.0%"},
+    "secundaria": null,
+    "prop_jugador": null
+  }
+];
+const laligaPicks = [];
+const eredivisiePicks = [];
+const oldResults = [];
+const mejores = [];
+const parlaysData = [];
+const todayResultsArray = [];
